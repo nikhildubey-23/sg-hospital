e6158e9002bf28ecc382df92058184d6abefa463
nikhildubey-23 <ravannick0089@gmail.com>
2026-05-14 12:05:55 +0530

    push time

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 // Dr. Sushmita's Clinic - Updated JavaScript (Bootstrap Compatible)
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = '/api';
 
 // Initialize Bootstrap components
 let appointmentModal;
